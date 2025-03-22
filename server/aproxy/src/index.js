0dae2f0c421c892b8d4f98a79c2cca23f6c881e7
Wei Guo <me@imkira.com>
2025-03-23 00:12:24 +0800

    Add proxy for steam

--- server/aproxy/src/index.js
+++ server/aproxy/src/index.js
@@ -9,7 +9,64 @@
  */
 
 export default {
+	async proxy(request, hostname) {
+		const url = new URL(request.url);
+		url.hostname = hostname;
+		url.protocol = 'https:';
+		url.port = 443;
+
+		const headers = new Headers(request.headers);
+		headers.set('Host', url.hostname);
+
+		const newRequest = new Request(url.toString(), {
+			method: request.method,
+			headers: headers,
+			body: request.body,
+			redirect: 'follow'
+		});
+
+		const response = await fetch(newRequest);
+		return new Response(response.body, {
+			status: response.status,
+			statusText: response.statusText,
+			headers: response.headers
+		});
+	},
+
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		const proxyAuthToken = ""
+		if (proxyAuthToken != "" && request.headers.get("Proxy-Auth-Token") != proxyAuthToken) {
+			return new Response('Invalid proxy auth token', { status: 401 })
+		}
+
+		const url = new URL(request.url);
+		if (url.pathname == '/mobileconf/getlist' || url.pathname == '/mobileconf/multiajaxop') {
+			try {
+				console.log("Input conf: " + url.pathname)
+				return await this.proxy(request, 'steamcommunity.com')
+			} catch (error) {
+				console.log("Unknown conf error: " + error)
+				return new Response('{"success": false, "message": "Unknown aproxy error"}', {
+					status: 500,
+				});
+			}
+		}
+
+		if (url.pathname.startsWith("/IAuthenticationService/") || url.pathname.startsWith("/ITwoFactorService/")) {
+			try {
+				console.log("Input api: " + url.pathname)
+				return await this.proxy(request, 'api.steampowered.com')
+			} catch (error) {
+				console.log("Unknown api error: " + error)
+				return new Response('Unknown aproxy error', {
+					status: 500,
+					headers: {
+						"x-eresult": "0"
+					},
+				});
+			}
+		}
+
+		return new Response('Invalid path', { status: 404 })
 	},
 };
